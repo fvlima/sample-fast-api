8d9175aca008cf899249a3148d194cfecd3166c5
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-09-29 21:58:27 -0300

    Add user and password validator

--- sample_fast_api/authentication.py
+++ sample_fast_api/authentication.py
@@ -1,9 +1,12 @@
+from typing import Optional
+from datetime import timedelta, datetime
 from uuid import UUID
-
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import Header, HTTPException, status
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from sample_fast_api.apps.users.models import User
+from sample_fast_api.apps.users.schemas import pwd_context
+from sample_fast_api.config import settings
 
 
 class Authentication:
@@ -13,3 +16,20 @@ class Authentication:
             UUID(token)
         except ValueError:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not Authorized")
+
+
+async def authenticate_user(form_data):
+    user = await User.query.where(User.name == form_data.username).gino.first()
+    exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        if not user or not pwd_context.verify(form_data.password, user.password):
+            raise exception
+    except ValueError:
+        raise exception
+    return user
+
+
--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,9 +1,15 @@
 import uuid
-
+from collections import namedtuple
+from fastapi import HTTPException
+import nest_asyncio
 import pytest
 from fastapi import HTTPException
 
-from sample_fast_api.authentication import Authentication
+from sample_fast_api.apps.users.schemas import pwd_context
+from sample_fast_api.authentication import Authentication, authenticate_user
+from tests.apps.users.factories import UserFactory
+
+nest_asyncio.apply()
 
 
 def test_valid_authentication():
@@ -16,3 +22,37 @@ def test_valid_authentication():
 def test_invalid_authentication():
     with pytest.raises(HTTPException):
         Authentication("token")
+
+
+@pytest.mark.asyncio
+async def test_authenticate_user(client):
+    password_hash = pwd_context.hash("abc123")
+    new_user = await UserFactory().create(name="username", password=password_hash)
+
+    RequestFormMock = namedtuple("OAuth2PasswordRequestForm", ["username", "password"])
+    form_data = RequestFormMock("username", "abc123")
+    user = await authenticate_user(form_data)
+    assert user.id == new_user.id
+
+
+@pytest.mark.asyncio
+async def test_authenticate_user_ivalid_password(client):
+    await UserFactory().create(name="username", password="unhased-password")
+
+    RequestFormMock = namedtuple("OAuth2PasswordRequestForm", ["username", "password"])
+    form_data = RequestFormMock("username", "abc123")
+    with pytest.raises(HTTPException) as exinfo:
+        await authenticate_user(form_data)
+    assert "Incorrect username or password" == exinfo.value.detail
+
+
+@pytest.mark.asyncio
+async def test_authenticate_user_ivalid_username(client):
+    password_hash = pwd_context.hash("abc123")
+    await UserFactory().create(name="name", password=password_hash)
+
+    RequestFormMock = namedtuple("OAuth2PasswordRequestForm", ["username", "password"])
+    form_data = RequestFormMock("username", "abc123")
+    with pytest.raises(HTTPException) as exinfo:
+        await authenticate_user(form_data)
+    assert "Incorrect username or password" == exinfo.value.detail
